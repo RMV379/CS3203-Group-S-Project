98fdc6682212c650a8d28279782afd379eeadfe4
RMV379 <ryan.m.vavricka-1@ou.edu>
2023-10-19 14:43:00 -0500

    added Unit test, removed UserInput.cpp

---
+++ UnitTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+
+public class UnitTest {
+
+    @Test
+    public void tempTest() { //test edge case, negative value target
+        Assertions.assertEquals(1, 1);
+    }
+    
+}
