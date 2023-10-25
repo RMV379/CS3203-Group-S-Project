b44e2f8458fa273960a66207fe239a92b0fcdcca
RMV379 <ryan.m.vavricka-1@ou.edu>
2023-10-25 11:18:09 -0500

    updated UnitTest comment

--- UnitTest.java
+++ UnitTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Assertions;
 public class UnitTest {
 
     @Test
-    public void tempTest() { //test edge case, negative value target
+    public void tempTest() { //TODO create tests
         Assertions.assertEquals(1, 1);
     }
     
