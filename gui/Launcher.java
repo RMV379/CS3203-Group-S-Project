aa1fbe639ab46d2614f99e10a5b085bf60e26fb1
tonyewest <tharcourt05@ou.edu>
2023-11-25 12:04:10 -0600

    Modified Launcher Class for Login(only takes credentials username and password)

--- gui/Launcher.java
+++ gui/Launcher.java
@@ -1,16 +1,66 @@
 package gui;
 
-import javax.swing.SwingUtilities;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.*;
 
-public class Launcher {
+public class Launcher extends JFrame {
+	
+	private JTextField usernameField;
+	private JPasswordField passwordField;
+	
+	public Launcher() {
+		setTitle("Log In");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setSize(300,200);
+		setLocationRelativeTo(null);
+		setLayout(new GridLayout(3,2));
+		
+		// Username Field
+		JLabel usernameLabel = new JLabel("Username");
+		usernameField = new JTextField();
+		add(usernameLabel);
+		add(usernameField);
+		
+		// Password Field
+		JLabel passwordLabel = new JLabel("Password");
+		passwordField = new JPasswordField();
+		add(passwordLabel);
+		add(passwordField);
+		
+		JButton loginButton = new JButton("Log In");
+		loginButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				String username = usernameField.getText();
+				char[] password = passwordField.getPassword();
+				String passwordString = new String(password);
+				
+				// TODO: Authentication for password
+				
+				if (username.equals("username") && passwordString.equals("password")) {
+					JOptionPane.showMessageDialog(null, "Logged in");
+					
+					// TODO: Actions after successful login
+				}
+				
+				else {
+					JOptionPane.showMessageDialog(null, "Username or Password is invalid.");
+				}
+			}
+		});
+		
+		add(loginButton);
+		
+		setVisible(true);
+	}
 
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
 			
 			@Override
 			public void run() {
-				MainWindow main = new MainWindow();
-				main.show();
+				new Launcher();
 			}
 		});
 	}
