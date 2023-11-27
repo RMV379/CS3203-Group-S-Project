fe0eb1aabd68b7ced089f5f8c0cd7e47db10c141
Parker Hix <parker.e.hix@gmail.com>
2023-11-27 03:54:05 -0600

    Added SQL table for posts, establishing many-to-one relationship with user

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -3,3 +3,10 @@ create table if not exists USERS (
     userName varchar(255),
     data binary large object
 );
+
+CREATE TABLE IF NOT EXISTS POSTS (
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    content TEXT,
+    user_id INT,
+    FOREIGN KEY (user_id) REFERENCES USERS(id)
+);
